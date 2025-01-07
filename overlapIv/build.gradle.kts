f672ef53c775fa0b4fcb20c817ecaced301ad0db
Aahan <aahan.verma@appinventiv.com>
2025-01-07 15:03:23 +0530

    Gradle Issues Fixed

--- overlapIv/build.gradle.kts
+++ overlapIv/build.gradle.kts
@@ -34,9 +34,13 @@ android {
 
 dependencies {
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
+    api(libs.androidx.core.ktx)
+    api(libs.androidx.appcompat)
+    api(libs.material)
+
     implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
